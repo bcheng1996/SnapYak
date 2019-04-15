2b017065bc03e634e82d66775f6ed6fde02462e6
Brian Guevara <brian.guevara0@gmail.com>
2019-04-15 15:38:57 -0400

    base project
    Contains the base project files

---
+++ SnapYak/SnapYak/FirebaseModel.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseModel.swift
+//  SnapYak
+//
+//  Created by Brian Guevara on 4/15/19.
+//  Copyright © 2019 group34. All rights reserved.
+//
+
+import Foundation
---
+++ SnapYak/SnapYak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapYak
+//
+//  Created by Brian Guevara on 4/15/19.
+//  Copyright © 2019 group34. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
