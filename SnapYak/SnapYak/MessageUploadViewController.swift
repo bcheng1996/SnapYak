366cf97a93350e17bdd21f86126039cca42ba212
Benny Cheng <bcheng1996@gmail.com>
2019-04-16 12:28:01 -0400

    beta uploadImageView

--- SnapYak/SnapYak/MessageUploadViewController.swift
+++ SnapYak/SnapYak/MessageUploadViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Photos
 
-class MessageUploadViewController: UIViewController {
+class MessageUploadViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var uploadButtonOutlet: UIButton!
+    @IBAction func uploadButtonAction(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.allowsEditing = true
+        vc.delegate = self
+        present(vc, animated: true)
+    }
+    @IBOutlet weak var imageOutlet: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -26,5 +37,18 @@ class MessageUploadViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension MessageUploadViewController {
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        
+        // print out the image size as a test
+        imageOutlet.image = image
+    }
 }
