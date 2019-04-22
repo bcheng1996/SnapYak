07107f17d01053bbead4393c0bc65271225cb2d2
Brian Guevara <brianguevara@Brians-MBP.fios-router.home>
2019-04-21 20:55:39 -0400

    v1.0 Messages List View and Message View

---
+++ SnapYak/SnapYak/MessageCell.swift
@@ -0,0 +1,27 @@
+//
+//  MessageCell.swift
+//  SnapYak
+//
+//  Created by Brian Guevara on 4/21/19.
+//  Copyright © 2019 group34. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    @IBOutlet var headlineLabel: UILabel!
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var votesLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
